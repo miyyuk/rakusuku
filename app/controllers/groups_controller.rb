f65d56175ec62c57e90b3a343676f0b7ac70c76f
miyyuk <orion.my.2@icloud.com>
2020-05-09 21:55:22 +0900

    グループチャットページへのルーティング設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   def index
+    @groups = current_user.groups.all
   end
 
   def new
@@ -10,7 +11,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to posts_path
+      redirect_to groups_path
     else
       render :new
     end
@@ -23,7 +24,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to posts_path
+      redirect_to group_posts_path
     else
       render :edit
     end
