868e02e9e8fc306b6a2194b69077af5da5524b00
miyyuk <orion.my.2@icloud.com>
2020-05-10 22:02:01 +0900

    コメント投稿・保存機能実装
    comments_controllerのcreateメソッドを設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to group_post_path(@comment.post.id)
+    else
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    # @comment = Comment.new
-    # @comments = @post.comments.includes(:user)
+    @comment = Comment.new
+    @comments = @post.comments.includes(:user)
   end
 
   def create
