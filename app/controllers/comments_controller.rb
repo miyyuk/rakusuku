06a55716b8b69be7142b382845c0e28a148bee1f
miyyuk <orion.my.2@icloud.com>
2020-05-11 11:16:23 +0900

    保存したコメントの表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :set_post
 
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @post.comments.new(comment_params)
     if @comment.save
-      redirect_to group_post_path(@comment.post.id)
+      redirect_to group_post_path(id: @post.id)
     else
+      # errorメッセージの表示を追加予定
+      @comments = @post.comments.includes(:user)
+      redirect_to group_post_path(id: @post.id)
     end
   end
 
   private
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
