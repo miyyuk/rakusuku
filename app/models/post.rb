5967f918ae47327772cc1cd524f7be3d707a9c40
miyyuk <orion.my.2@icloud.com>
2020-05-11 18:15:04 +0900

    アソシエーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :group
   belongs_to :user
   has_many :comments
+  has_many :looks
+  has_many :looked_posts, through: :looks, source: :post
 
   validates :content, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users
   has_many :posts
   has_many :comments
+  has_many :looks
+  has_many :looked_posts, through: :looks, source: :post
 
   validates :firstname, presence: true
   validates :lastname, presence: true
