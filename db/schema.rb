7fade2bbfbecaa49b8474fac36f059d20a3a0f62
miyyuk <orion.my.2@icloud.com>
2020-05-11 18:12:53 +0900

    looksテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_074417) do
+ActiveRecord::Schema.define(version: 2020_05_11_091149) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content", null: false
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2020_05_11_074417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_groups_on_name", unique: true
+  end
+
+  create_table "looks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_looks_on_post_id"
+    t.index ["user_id"], name: "index_looks_on_user_id"
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -73,6 +82,8 @@ ActiveRecord::Schema.define(version: 2020_05_11_074417) do
   add_foreign_key "comments", "users"
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+  add_foreign_key "looks", "posts"
+  add_foreign_key "looks", "users"
   add_foreign_key "posts", "groups"
   add_foreign_key "posts", "users"
 end
