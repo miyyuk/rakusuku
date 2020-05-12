5a59c7cf08248d1c314cb4fbe1c4da8cdbcb8010
miyyuk <orion.my.2@icloud.com>
2020-05-12 12:09:01 +0900

    lookの作成・削除機能の設定

--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -1,7 +1,19 @@
 class LooksController < ApplicationController
+  before_action :set_post
+
   def create
+    @look = current_user.looks.create(post_id: @post.id)
+    redirect_to group_posts_path(id: @post.id)
   end
 
   def destroy
+    @look = Look.find_by(post_id: @post.id, user_id: current_user.id)
+    @look.destroy
+    redirect_to group_posts_path(id: @post.id)
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
