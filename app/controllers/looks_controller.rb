b3bab199a942a0dcf0037cc4361bef6b3dd08707
miyyuk <orion.my.2@icloud.com>
2020-05-12 17:31:41 +0900

    見ました機能のajax化実装（未完）
    post,lookを部分テンプレート化
    create.js.hamlとdestroy.js.hamlでajax化するもエラーが解決せず。
    look自体はcreate,destroyともに正常に行われており、リロードすれば表示もされるため、一旦このままにして他機能を実装する。

--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -1,19 +1,10 @@
 class LooksController < ApplicationController
-  before_action :set_post
-
   def create
-    @look = current_user.looks.create(post_id: @post.id)
-    redirect_to group_posts_path(id: @post.id)
+    @look = current_user.looks.create(post_id: params[:post_id])
   end
 
   def destroy
-    @look = Look.find_by(post_id: @post.id, user_id: current_user.id)
+    @look = Look.find_by(post_id: params[:post_id], user_id: current_user.id)
     @look.destroy
-    redirect_to group_posts_path(id: @post.id)
-  end
-
-  private
-  def set_post
-    @post = Post.find(params[:post_id])
   end
 end
