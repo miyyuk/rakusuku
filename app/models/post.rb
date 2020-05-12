9cead7f844c25545090357a58bfa1c1f98723221
miyyuk <orion.my.2@icloud.com>
2020-05-12 12:06:40 +0900

    postモデルの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecord
   belongs_to :group
   belongs_to :user
   has_many :comments
-  has_many :looks
-  has_many :looked_posts, through: :looks, source: :post
+  has_many :looks, dependent: :destroy
+  has_many :looked_users, through: :looks, source: :post
 
   validates :content, presence: true
 
