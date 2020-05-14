d2b195f1d3af037192d99af8db5dc6243f2427aa
miyyuk <orion.my.2@icloud.com>
2020-05-14 20:55:35 +0900

    アソシエーションの変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :group
   belongs_to :user
+  has_many :post_files, dependent: :destroy
   has_many :comments
   has_many :looks, dependent: :destroy
+  has_many :post_files, dependent: :destroy
+  accepts_nested_attributes_for :post_files
 
   validates :content, presence: true
-
-  mount_uploaders :files, ImageUploader
 end
