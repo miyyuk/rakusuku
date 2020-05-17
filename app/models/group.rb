3203a8f1c8b220d549d8a865c6fcaf469a00ee73
miyyuk <orion.my.2@icloud.com>
2020-05-17 21:46:03 +0900

    グループ削除機能の実装
    外部キー設定のところにdependent::destroyを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
-  has_many :group_users
+  has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
