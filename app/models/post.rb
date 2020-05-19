8558a025b0302437297b7d260f064ade8a7d8ff8
miyyuk <orion.my.2@icloud.com>
2020-05-19 18:56:47 +0900

    各ビューの微修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :group
   belongs_to :user
-  has_many :post_files, dependent: :destroy
   has_many :comments
   has_many :looks, dependent: :destroy
   has_many :post_files, dependent: :destroy
