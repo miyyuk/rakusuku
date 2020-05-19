eb72081dc5252097d6b9a4d942cd6981a60b6ff9
miyyuk <orion.my.2@icloud.com>
2020-05-19 18:57:12 +0900

    見ました機能の微修正
    ajaxの実装はできたが、リロードしないとビューが反映されない。ajaxをなくせば自動でリロードしてくれるので、仮でajaxを解除している。

--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -1,17 +1,17 @@
 class LooksController < ApplicationController
   before_action :set_group
-  
+
   def create
     @post = Post.find(params[:post_id])
     look = current_user.looks.create(post_id: params[:post_id])
-    # redirect_to group_posts_path(group_id: @group.id)
+    redirect_to group_posts_path(group_id: @group.id)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     look = Look.find_by(post_id: params[:post_id], user_id: current_user.id)
     look.destroy
-    # redirect_to group_posts_path(group_id: @group.id)
+    redirect_to group_posts_path(group_id: @group.id)
   end
 
   private
